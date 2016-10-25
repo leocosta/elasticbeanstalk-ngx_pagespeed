07d939a2eac0d957d6efac6b746957fbf5a36788
Leonardo Costa <leoccosta@outlook.com>
2016-10-25 10:43:57 -0200

    Configure modules for nginx

--- elasticbeanstalk-lemp.sh
+++ elasticbeanstalk-lemp.sh
@@ -19,11 +19,11 @@ tar -xzvf ${NPS_VERSION}.tar.gz
 cd
 
 # Install Nginx
-NGINX_VERSION=1.8.0
+NGINX_VERSION=1.8.1
 wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 tar -xvzf nginx-${NGINX_VERSION}.tar.gz
 cd nginx-${NGINX_VERSION}/
-./configure --add-module=$HOME/ngx_pagespeed-release-${NPS_VERSION}-beta --with-http_gzip_static_module --with-http_realip_module --with-http_ssl_module
+./configure --add-module=/home/ec2-user/ngx_pagespeed-release-${NPS_VERSION}-beta --with-http_gzip_static_module --with-http_realip_module --with-http_ssl_module --http-client-body-temp-path=/var/lib/nginx/tmp/client_body --http-proxy-temp-path=/var/lib/nginx/tmp/proxy --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi --http-scgi-temp-path=/var/lib/nginx/tmp/scgi --pid-path=/var/run/nginx.pid --lock-path=/var/lock/subsys/nginx --user=nginx --group=nginx --with-ipv6 --with-http_ssl_module --with-http_spdy_module --with-http_realip_module --with-http_addition_module  --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_stub_status_module --with-mail --with-mail_ssl_module --with-pcre --with-pcre-jit --with-debug --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic' --with-ld-opt=' -Wl,-E'
 make
 make install
 cd
@@ -39,8 +39,8 @@ chmod +x /etc/init.d/nginx
 chkconfig nginx on
 
 # Remove install files
-/bin/rm -rf /root/nginx-1.8.0
-/bin/rm -rf /root/nginx-1.8.0.tar.gz
+/bin/rm -rf /root/nginx-1.8.1
+/bin/rm -rf /root/nginx-1.8.1.tar.gz
 /bin/rm -rf /root/ngx_pagespeed-release-1.9.32.4-beta
 /bin/rm -rf /root/release-1.9.32.4-beta.zip
 
